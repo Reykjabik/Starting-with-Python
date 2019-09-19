608e4613db80370089004be73ac18c61a525c6bc
Javier <great_jabibip@msn.com>
2019-09-19 20:20:39 +0300

    Add files via upload

---
+++ mapIt.py
@@ -0,0 +1,23 @@
+#! python3
+''' This program takes an address (whether from the command line or from the clipboard)
+    and searches for it automatically in Google Maps.
+
+    NOTE: If known, please add the city at the end, as sometimes Google cannot show you
+    a street straight away if it is found on many cities accross the world.
+
+    Example:
+
+    mapIt.py tverskaya 11, Moscow
+'''
+
+import webbrowser, sys, pyperclip   # webbrowser: to open browser
+                                    # sys: to read command line arguments
+                                    # to have access to the clipboard
+
+
+if len(sys.argv) > 1:                   # if there are arguments in the command line
+    address = ' '.join(sys.argv[1:])
+else:                                   # otherwise, get the address from the clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
